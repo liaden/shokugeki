8b81a8dd984aeb95da89126971bcc885660a2b5b
Joel Johnson <johnson.joel.b@gmail.com>
2016-04-10 19:25:27 +0000

    Add addtional recipes to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,7 +69,103 @@
         'ketchup',
         'balsamic vinegar'
       )
-  )
+  ),
+  Recipe.create!(
+    name: 'Rhubarb Crumble',
+    url: 'http://www.foodnetwork.com/recipes/food-network-kitchens/rhubarb-crumble-recipe.html',
+    ingredients:
+      Ingredient.mass_produce(
+        'flour',
+        'oats',
+        'sugar',
+        'butter',
+        'hazelnuts',
+        'rhubarb',
+        'vanilla extract',
+        'orange zest',
+        'salt'
+      )
+  ),
+  Recipe.create!(
+    name: 'Quick, Spicy Leek and Shrimp Stir-Fry',
+    url: 'http://www.foodnetwork.com/recipes/food-network-kitchens/quick-spicy-leek-and-shrimp-stir-fry.html',
+    ingredients:
+      Ingredient.mass_produce(
+        'leeks',
+        'vegetable oil',
+        'salt',
+        'black pepper',
+        'red pepper flakes',
+        'shrimp',
+        'garlic'
+      )
+  ),
+  Recipe.create!(
+    name: 'Good Eats Meatloaf',
+    url: 'http://www.foodnetwork.com/recipes/alton-brown/good-eats-meat-loaf-recipe.html',
+    ingredients:
+      Ingredient.mass_produce(
+        'croutons',
+        'black pepper',
+        'cayenne pepper',
+        'chili powder',
+        'thyme',
+        'onion',
+        'carrot',
+        'garlic',
+        'red bell pepper',
+        'ground chuck',
+        'ground sirloin',
+        'salt',
+        'egg'
+      )
+  ),
+  Recipe.create!(
+    name: 'Guacamole',
+    url: 'http://www.foodnetwork.com/recipes/alton-brown/guacamole-recipe.html',
+    ingredients:
+      Ingredient.mass_produce(
+        'avocados',
+        'lime juice',
+        'salt',
+        'cumin',
+        'cayenne',
+        'onion',
+        'jalapeno',
+        'roma tomatoes',
+        'cilantro',
+        'garlic'
+      )
+  ),
+  Recipe.create!(
+    name: 'French Toast',
+    url: 'http://www.foodnetwork.com/recipes/alton-brown/french-toast-recipe.html',
+    ingredients:
+      Ingredient.mass_produce(
+        'half and half',
+        'egg',
+        'honey',
+        'salt',
+        'brioche',
+        'butter'
+      )
+  ),
+  Recipe.create!(
+    name: 'Hot Spinach and Artichoke Dip',
+    url: 'http://www.foodnetwork.com/recipes/alton-brown/hot-spinach-and-artichoke-dip-recipe.html',
+    ingredients:
+      Ingredient.mass_produce(
+        'spinach',
+        'artichoke hearts',
+        'cream cheese',
+        'sour cream',
+        'mayonnaise',
+        'parmesan', 
+        'red pepper flakes',
+        'salt',
+        'garlic'
+      )
+    )
 ].each do |recipe|
   RecordPairings.run!(recipe: recipe)
 end
